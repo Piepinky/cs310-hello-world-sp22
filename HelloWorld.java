76fa9a52ab53d9ebe81ace1c4d31187d0c330615
Tion <wrighttion124@gmail.com>
2022-01-28 13:31:46 -0500

    Added v3.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,7 +13,10 @@ public class HelloWorld {
     private void f1() {
         // v1.0
     }
-  private void f2() {
+    private void f2() {
         // v2.0
+    }
+    private void f3() {
+        // v3.0
     }
 }
